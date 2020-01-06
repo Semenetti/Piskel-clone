e405f6fd196c2048e78282d64e415462c4a12313
Semenetti <adriano.semenetti@gmail.com>
2020-01-06 21:41:41 +0300

    feat: add download file as PNG

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import { drawCanvas } from "./change_size.js";
 import { autoDrawing } from "./pen.js";
 import { onloadGrid } from "./new_frames.js";
 import { printNumbers } from "./video_animation.js";
+import { exportCanvasAsPNG } from "./save_image.js";
 
 window.onload = () => {
   drawCanvas(20);
   autoDrawing();
   onloadGrid();
 };
-
---
+++ save_image.js
@@ -0,0 +1,24 @@
+export function exportCanvasAsPNG(id, fileName) {
+  var canvasElement = document.getElementById(id);
+
+  var MIME_TYPE = "image/png";
+
+  var imgURL = canvasElement.toDataURL(MIME_TYPE);
+
+  var dlLink = document.createElement("a");
+  dlLink.download = fileName;
+  dlLink.href = imgURL;
+  dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(
+    ":"
+  );
+
+  document.body.appendChild(dlLink);
+  dlLink.click();
+  document.body.removeChild(dlLink);
+}
+
+let download = document.getElementById("download");
+
+download.addEventListener("click", () => {
+  exportCanvasAsPNG("canvas_block", "file");
+});
