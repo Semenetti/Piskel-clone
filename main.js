ea5b45a798498310b29a2f91dcee0634520308d6
Semenetti <adriano.semenetti@gmail.com>
2020-01-08 22:24:29 +0300

    refactor

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-import { drawCanvas } from "./change_size.js";
-import { autoDrawing, clearClassName, pen } from "./pen.js";
-import { onloadGrid, toClearCanvas } from "./new_frames.js";
-import { printNumbers } from "./video_animation.js";
-import { exportCanvasAsPNG } from "./save_image.js";
-import { createCanvas } from "./animation_preview.js";
-import { changeBackground, bucket } from "./bucket.js";
-import { pickColor, eyeDropper } from "./eye_dropper.js";
+import { drawCanvas } from './change_size.js';
+import { autoDrawing, clearClassName, pen } from './pen.js';
+import { onloadGrid, toClearCanvas } from './new_frames.js';
+import { printNumbers } from './video_animation.js';
+import { exportCanvasAsPNG } from './save_image.js';
+import { createCanvas } from './animation_preview.js';
+import { changeBackground, bucket } from './bucket.js';
+import { pickColor, eyeDropper } from './eye_dropper.js';
 
 window.onload = () => {
   drawCanvas(20);
   autoDrawing();
   onloadGrid();
-  document.addEventListener("keydown", event => {
+  document.addEventListener('keydown', event => {
     switch (event.code) {
-      case "KeyP":
+      case 'KeyP':
         clearClassName();
-        pen.className = "Selected";
+        pen.className = 'Selected';
         autoDrawing();
         break;
-      case "KeyB":
+      case 'KeyB':
         clearClassName();
-        bucket.className = "Selected";
+        bucket.className = 'Selected';
         changeBackground();
         break;
-      case "KeyE":
+      case 'KeyE':
         clearClassName();
-        eyeDropper.className = "Selected";
+        eyeDropper.className = 'Selected';
         pickColor();
         break;
-      case "KeyC":
+      case 'KeyC':
         toClearCanvas();
         break;
       default:
